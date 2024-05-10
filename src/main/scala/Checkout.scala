181a90e211c4a13690ebdd0944033a0a860f7437
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2024-05-10 18:45:53 +0100

    Move discount and fruit count logic to own methods

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -2,17 +2,31 @@ import objects.{Apple, Orange}
 object Checkout {
   def CheckPrices(shoppingList: List[String]): Double = {
 
-    val numOfOranges = shoppingList.count(_ == "Orange")
-    val numberOfApples = shoppingList.count(_ == "Apple")
+    val numOfOranges = checkOranges(shoppingList)
+    val numberOfApples = checkApples(shoppingList)
 
+    val totalCost: Double = applyDiscount(numOfOranges, numberOfApples)
+    totalCost
+  }
+
+  private def checkApples(shoppingList: List[String]): Int = {
+    val appleCount = shoppingList.count(_ == "Apple")
+    appleCount
+  }
+
+  private def checkOranges(shoppingList: List[String]): Int = {
+    val orangeCount = shoppingList.count(_ == "Orange")
+    orangeCount
+  }
+
+  private def applyDiscount(numOfOranges: Int, numberOfApples: Int): Double = {
     val discountedOranges: Int = numOfOranges / 3
     val discountedApples: Int = numberOfApples / 2
 
     val totalOrangePrice = (numOfOranges - discountedOranges) * Orange.price
     val totalApplePrice = (numberOfApples - discountedApples) * Apple.price
 
-    val totalCost: Double = totalOrangePrice + totalApplePrice
-
-    totalCost
+    val totalDiscountedCost: Double = totalOrangePrice + totalApplePrice
+    totalDiscountedCost
   }
 }
